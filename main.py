7272fa3cb46282b6a52f3bcfdbeb1aee84f2eb86
Ikigami Sama <ikigamidevs.15@gmail.com>
2026-06-18 19:15:10 +0800

    Fixes CustomerListProvider

--- main.py
+++ main.py
@@ -42,6 +42,24 @@ schema = [
         "key_label": "template",
         "options": {"template": "{{id}} {{IP Address}}"}
     },
+
+    {
+        "label": "temp_ip_address",
+        "key_label": "customer_list",
+        "options": {"custom_format": "QWE,ASD,ZXC"}
+    },
+    {
+        "label": "temp_ip_address",
+        "key_label": "customer_list",
+        "options": {"values": "{{id}} {{IP Address}}"}
+    },
+
+
+    {
+        'label': 'order_date',
+        'key_label': 'datetime',
+        "options": {'from_date': "01/01/2020", 'to_date': '12/31/2025', 'date_format': 'ISO 8601 (UTC)'}
+    },
     {
         "label": "created_at",
         "key_label": "current_timestamp",
--- src/ikidatagen/providers/basic/custom_list.py
+++ src/ikidatagen/providers/basic/custom_list.py
@@ -2,9 +2,20 @@ from ..base_provider import BaseProvider
 
 
 class CustomListProvider(BaseProvider):
-    def __init__(self, blank_percentage: float = 0.0, custom_format: str = '', **kwargs):
+    def __init__(
+        self,
+        blank_percentage: float = 0.0,
+        values=None,
+        **kwargs
+    ):
         super().__init__(blank_percentage=blank_percentage, **kwargs)
-        self.custom_format = custom_format.split(',')
+
+        if isinstance(values, list):
+            self.custom_format = values
+        elif isinstance(values, str):
+            self.custom_format = values.split(',')
+        else:
+            self.custom_format = []
 
     def generate_non_blank(self, row_data=None):
         return self.get_random_data_by_list(self.custom_format)
